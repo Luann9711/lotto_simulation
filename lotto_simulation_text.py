25f2705e80f72cbf9e9d240b50262338a157ec82
luann9711 <marchen9708@gmail.com>
2021-12-15 22:31:26 +0900

    create the bonus num RNG
    got an error with appending bonus num in to the original Random number list.
    
    not try make a plus in the print func
    try use append() to fix it

--- lotto_simulation_text.py
+++ lotto_simulation_text.py
@@ -4,7 +4,22 @@ def generate_numbers(n):
     number = list(range(1,46))
     random_number = random.sample(number, n)
 
-    print(sorted(random_number))
+    return sorted(random_number)
 
 
-generate_numbers(6)
+def draw_winning_numbers():
+    winning_numbers = generate_numbers(6)
+    number = list(range(1,46))
+    bonus_number_list = []
+    
+    for i in number:
+        if i not in winning_numbers:
+            bonus_number_list += [i]
+    
+    bonus_number = random.choice(bonus_number_list)
+    winning_numbers.append(bonus_number)
+    
+    print(winning_numbers)
+
+draw_winning_numbers()
+
